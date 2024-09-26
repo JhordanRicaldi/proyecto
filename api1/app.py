1c834dc4667facab90ad8dd1d7d3a856082a2a83
Jhordan <77322350@continental.edu.pe>
2024-09-26 10:42:41 -0500

    se optimizo el modelo h5

--- api1/app.py
+++ api1/app.py
@@ -4,14 +4,29 @@ import tensorflow as tf
 import numpy as np
 from PIL import Image
 import io
+import h5py
 
 app = Flask(__name__)
 CORS(app)
 
-# Cargar el modelo previamente entrenado
-model = tf.keras.models.load_model('modelo_alimentos.h5')
+# Función para cargar el modelo en dos partes
+def load_model_in_parts():
+    model = tf.keras.models.Model()
+    with h5py.File('modelo_alimentos_parte1.h5', 'r') as f1, h5py.File('modelo_alimentos_parte2.h5', 'r') as f2:
+        for key in f1.keys():
+            weight = f1[key][:]
+            model.add(tf.keras.layers.Dense(weight.shape[1], input_shape=(weight.shape[0],)))
+            model.layers[-1].set_weights([weight])
+        for key in f2.keys():
+            weight = f2[key][:]
+            model.add(tf.keras.layers.Dense(weight.shape[1]))
+            model.layers[-1].set_weights([weight])
+    return model
 
-# Función para procesar la imagen
+# Cargar el modelo
+model = load_model_in_parts()
+
+# El resto de tu código permanece igual
 def load_and_preprocess_image(image):
     image = image.resize((224, 224))
     image = np.array(image) / 255.0
@@ -53,5 +68,4 @@ def info():
     })
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5001)  # Puedes cambiar "5001" por el puerto que prefieras
-
+    app.run(debug=True, host='127.0.0.1', port=5001)
